2bd6f73fd1a3c626c8d05526ad5f97d408552ce5
JONATHAN_SWIFT <juniorjonswift@gmail.com>
2024-05-08 14:10:42 +0100

    added new stuff

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import json
+import threading
 from pokemontcgsdk import Card, Set, Type, Supertype, Subtype, Rarity, RestClient
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import requests
 import os
 from currency_converter import CurrencyConverter
+import sys
 
 c = CurrencyConverter()
 
@@ -10,29 +13,85 @@ app = Flask(__name__)
 
 RestClient.configure('5b9f6767-a8c8-4bcd-be90-81fa1c2a0178')
 
+global card_to_download
+global CurrentCard
+global CurrentCardName
+
+CurrentCard = ''
+CurrentCardName = ''
+
+def DownloadFile(card):
+    global card_to_download
+    cardImg = card.images.large
+    Filename = card.id+'.png'
+    try:
+        r = requests.get(cardImg)
+        print(r)
+        with open('./static/img/'+Filename, 'wb') as f:
+            f.write(r.content)
+    except Exception as e:
+        print('exception downloading URL: ', e)
+
+def DownloadPhotoOld(card):
+    cardImg = card.images.large
+    res = requests.get(cardImg)
+    global images
+    print("Card Not Found, Downloading")
+    with open('static/img/' + card.id + '.png', 'wb') as f:
+        f.write(res.content)
+        images.append(f)
+        f.close()
+
 @app.route('/')
 def homepage():
     return render_template('index.html')
 
 @app.route('/', methods=['POST'])
 def loadPokemon():
-    print(request.method)
+    global CurrentCard
+    global CurrentCardName
+    global images
     if request.form.get('Search') == 'Search':
         text = request.form['Card']
         cards = Card.where(q=f'name:{text}')
-        images = []
+
+        CurrentCard = cards
+        CurrentCardName = text
         for card in cards:
-            cardImg = card.images.small
-            res = requests.get(cardImg)
-            card.cardmarket.prices.averageSellPriceGBP = round(
-                c.convert(card.cardmarket.prices.averageSellPrice, 'USD', 'GBP'), 2)
-            if os.path.isfile('static/img/' + card.id + '.png'):
-                images.append(open('static/img/' + card.id + '.png', 'r'))
+            card.id = card.id.replace("-", "$")
+            if os.path.isfile('./static/img/'+card.id+'.png'):
+                card.isCached = True
             else:
-                print("Card Not Found, Downloading")
-                with open('static/img/' + card.id + '.png', 'wb') as f:
-                    f.write(res.content)
-                    images.append(f)
-        return render_template("results.html", Cards=cards, search=text, images=images, itemsfound=len(cards))
+                card.isCached = False
+            card.cardPrice = 0.0
+            cardPriceGBP = round(c.convert(card.cardPrice, 'USD', 'GBP'), 2)
+            card.cardPriceGBP = cardPriceGBP
+        return render_template("results.html", Cards=cards, search=text, itemsfound=len(cards), css='./static/results.css')
     if request.form.get('Home') == 'Home':
         return render_template('index.html')
+
+@app.route('/ViewCard')
+def viewCard():
+
+    keptTraits = ['abilities', 'attacks', 'cardmarket', 'evolvesFrom', 'hp', 'id', 'rarity', 'resistances']
+
+    cardId = request.args.get('data')
+    cardId = cardId.replace("$", "-")
+
+    card = Card.find(cardId)
+    
+    shownAttributes = []
+
+    for attr in dir(card):
+        if attr.lower() in keptTraits:
+            shownAttributes.append(attr)
+    
+    if card.abilities != None:
+        card.hasAbilities = True
+    else:
+        card.hasAbilities = False
+    
+    for attack in card.attacks:
+        print(attack.cost)
+
+    return render_template('cardView.html', card=card, shownAttributes=shownAttributes)
---
+++ static/js/redirect.js
@@ -0,0 +1,8 @@
+function showCard(value) {
+    console.log(value);
+    window.location.replace('ViewCard?data='+value);
+}
+
+function SendHome(){
+    window.location.replace('/')
+}
